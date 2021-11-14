66dd75565013c028174cff3b918201766d084900
Sungkur <yash.sungkur@accenture.com>
2021-11-14 17:43:55 +0400

    Seventh commit

--- Selenium.py
+++ Selenium.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
 
-webdriver.Chrome().get('https://allcamping-gears.com/camping-tents/')
+chrome_options = webdriver.ChromeOptions(); 
+chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']);
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.google.com/')
+
+search_box = driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+search_box.send_keys("camping tents allcamping-gears.com")
