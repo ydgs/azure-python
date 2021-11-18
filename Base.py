50a3a84c8d3538cc0e76fba769a9e4b3bc2534c4
Sungkur <yash.sungkur@accenture.com>
2021-11-18 22:19:31 +0400

    14 Commit

--- Base.py
+++ Base.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandWindows\1.1.9\Downloads\chromedriver.exe')
+#driver = webdriver.Chrome('C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandWindows\1.1.9\Downloads\chromedriver.exe')
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get('https://google.com/')
