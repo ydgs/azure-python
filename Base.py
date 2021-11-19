fdd955593fc152a115eafcafbfae0a506bb0610b
Sungkur <yash.sungkur@accenture.com>
2021-11-19 18:54:46 +0400

    16 Commit

--- Base.py
+++ Base.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\\Packages\\Plugins\\Microsoft.CPlat.Core.RunCommandWindows\\1.1.9\\Downloads\\chromedriver.exe')
+chrome_options = webdriver.ChromeOptions(); 
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=chrome_options, executable_path='C:\\Packages\\Plugins\\Microsoft.CPlat.Core.RunCommandWindows\\1.1.9\\Downloads\\chromedriver.exe')
 
 #from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get('https://google.com/')
